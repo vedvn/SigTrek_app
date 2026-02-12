6c7f90347433c5b8e6ce4d85305221ec449a440d
unknown <callforved@gmail.com>
2026-02-13 01:54:09 +0530

    changes done!

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,25 @@
-// ⚠️ Do NOT declare repositories here
-// ⚠️ Do NOT use buildscript {} block (handled in settings.gradle.kts)
-
-// Ensures :app is evaluated first
-subprojects {
-    evaluationDependsOn(":app")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+    }
 }
 
-// Standard clean task
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
